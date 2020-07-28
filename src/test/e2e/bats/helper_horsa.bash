eebde60cfb1a0f095c1c4510f706dc75db84b6f0
radu helstern <radu.helstern@gmail.com>
2020-07-28 02:40:46 +0100

    improve the bats test helpers use go 1.13 and 1.14 with travis

--- src/test/e2e/bats/helper_horsa.bash
+++ src/test/e2e/bats/helper_horsa.bash
@@ -3,3 +3,59 @@
 PROJECT_DIR="$( cd "$( dirname "${BATS_TEST_DIRNAME}/../../../../../" )" >/dev/null 2>&1 && pwd )"
 
 export HORSA=${PROJECT_DIR}/target/horsa/horsa
+
+# compares the default go version against a supplied one using a specified operator
+# it returnins the supplied version if the comparison returns true or an empty string
+# example: horsa_supported_go_version < 1.14
+function horsa_supported_go_version() {
+    local GO_VERSION; read GO_VERSION < <(go version | cut -d ' ' -f 3)
+
+    local GREATER_GO_VERSION; read GREATER_GO_VERSION < <(echo -e "go1.13\n${GO_VERSION}" | sort --version-sort | tail -n 1)
+    if test "${GO_VERSION}" = "${GREATER_GO_VERSION}"
+    then
+        echo ${GO_VERSION}
+    else
+        echo
+    fi
+}
+
+function horsa_compiled_version_compare() {
+    local OPERATOR=${1}
+    local VERSION=go${2}
+
+    local COMPILED_VERSION; read COMPILED_VERSION < <(go version ${HORSA} | cut -d ' ' -f 2)
+
+    if test "${COMPILED_VERSION}" = "${VERSION}" && test "${OPERATOR}" = "=" -o "${OPERATOR}" = ">=" -o "${OPERATOR}" = ">="
+    then
+        return 0
+    fi
+
+
+    if test "${OPERATOR}" = "=" -a "${COMPILED_VERSION}" != "${VERSION}"
+    then
+        return 1
+    fi
+
+    local GREATER_VERSION; read GREATER_VERSION < <(echo -e "${COMPILED_VERSION}\n${VERSION}" | sort --version-sort | tail -n 1)
+
+    case "${OPERATOR}" in
+        '>')
+            if test "${COMPILED_VERSION}" = "${GREATER_VERSION}"
+            then
+                return 0
+            else
+                return 1
+            fi
+        ;;
+        '<')
+            if test "${VERSION}" = "${GREATER_VERSION}"
+            then
+                return 0
+            else
+                return 1
+            fi
+        ;;
+    esac
+
+    exit 1
+}
--- src/test/e2e/bats/horsa.bats
+++ src/test/e2e/bats/horsa.bats
@@ -3,26 +3,35 @@
 load helper_horsa
 load helper_socket
 
-@test "invoking foo with a nonexistent file prints an error" {
+@test "test os resources are released after each connection is closed" {
+    local GOLANG_VERSION; read GOLANG_VERSION < <(horsa_supported_go_version)
+    test -n "${GOLANG_VERSION}"
+
+    # it appears that when compiled under go 1.14 after the initial connection, lsof reports two pipes
+    local EXPECTED_OPENPIPES=2
+    if horsa_compiled_version_compare '<' 1.14
+    then
+        EXPECTED_OPENPIPES=0
+    fi
+
     local SYSTEMD; read SYSTEMD < <(which systemd-socket-activate || echo /lib/systemd/systemd-activate)
 
     local PORT=8000
     ${SYSTEMD} --listen ${PORT} ${HORSA} systemd ${HORSA_ECHO_SERVER} &
     local SERVER_PID=$!
 
-    local OPENED_SOCKETS; read OPENED_SOCKETS < <(lsof -c horsa +E | grep -E '^horsa.+FIFO' | wc -l)
-    test 0 = ${OPENED_SOCKETS}
+    local OPEN_PIPES; read OPEN_PIPES < <(lsof -c horsa +E | grep -E '^horsa.+FIFO' | wc -l)
+    test 0 = ${OPEN_PIPES}
 
     local RESPONSE; read RESPONSE < <(echo "hello" | PORT=${PORT} socket_client)
-    read OPENED_SOCKETS < <(lsof -c horsa +E | grep -E '^horsa.+FIFO' | wc -l)
-    test 2 = ${OPENED_SOCKETS}
+    read OPEN_PIPES < <(lsof -c horsa +E | grep -E '^horsa.+FIFO' | wc -l)
+    test ${EXPECTED_OPENPIPES} = ${OPEN_PIPES}
     test 'hello' = "${RESPONSE}"
-
 
     read RESPONSE < <(echo "hello" | PORT=${PORT} socket_client)
-    read OPENED_SOCKETS < <(lsof -c horsa +E | grep -E '^horsa.+FIFO' | wc -l)
+    read OPEN_PIPES < <(lsof -c horsa +E | grep -E '^horsa.+FIFO' | wc -l)
     test 'hello' = "${RESPONSE}"
-    test 2 = ${OPENED_SOCKETS}
+    test ${EXPECTED_OPENPIPES} = ${OPEN_PIPES}
 
     kill ${SERVER_PID}
 }
